efff040622e976f5890992349ed61c5689b021fb
Ayanda Dlamini <Janicedoe745@gmail.com>
2025-08-28 14:22:53 +0200

    fixed the button

--- joke.js
+++ joke.js
@@ -1,31 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let jokeButtonElement = document.querySelector("#joke-btn");
-  let jokeElement = document.querySelector("#joke");
-
-  function generateJoke() {
-    let APIKey = "ad28f3a0557d8t5f574o89b184356e5a";
-    let context = "You are an AI that tells dad jokes";
-    let prompt = "Tell me a dad joke";
-
-    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${APIKey}`;
-
-    jokeElement.innerHTML = "😂 Generating a joke... please wait";
-
-    axios.get(apiURL)
-      .then(tellJoke)
-      .catch(function (error) {
-        jokeElement.innerHTML = "❌ Failed to fetch joke. Check console.";
-        console.error(error);
-      });
-  }
-
-  function tellJoke(response) {
-    jokeElement.innerHTML = "";
+function tellJoke(response) {
     new Typewriter(jokeElement, {
-      strings: [response.data.answer],
+      strings: response.data.answer,
       autoStart: true,
     });
   }
 
-  jokeButtonElement.addEventListener("click", generateJoke);
-});
+  function generateJoke() {
+
+    let APIKey = "ad28f3a0557d8t5f574o89b184356e5a";
+    let context = "You are a helpful assistant that generates IT Support , computers and everything IT jokes. create a short, funny, and clean joke.basic HTML and separate each line with a <br /> do not include ```html and show me different jokes everytime i ask;";
+    let prompt = "Tell me a IT support Joke tell me a different joke each time I ask.";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${APIKey}`;
+
+    //jokeElement.innerHTML = "😂 Generating a joke... please wait";
+
+    axios.get(apiURL).then(tellJoke)
+      
+  }
+
+  
+
+  let jokeButtonElement = document.querySelector("#joke_btn");
+
+  let jokeElement = document.querySelector("#joke");
+      jokeButtonElement.addEventListener("click", generateJoke);
+
